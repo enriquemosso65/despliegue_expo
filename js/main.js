e0a561763cd316838693faf27776c885879af57d
enriquemosso65 <enriquemosso65@aragon.unam.mx>
2021-06-15 13:32:29 -0500

    nuevo index, falta responsive

--- js/main.js
+++ js/main.js
@@ -127,3 +127,27 @@ $(function () {
 // Boton leer mas
 
 
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".hero", {
+  scrollTrigger: {
+    trigger: ".hero",
+    scrub: true,
+    pin: true,
+    start: "center center",
+    end: "bottom -100%",
+    toggleClass: "active",
+    ease: "power2"
+  }
+});
+
+gsap.to(".hero__image", {
+  scrollTrigger: {
+    trigger: ".hero",
+    scrub: 0.9,
+    start: "top bottom",
+    end: "bottom -1000%",
+    ease: "power2"
+  },
+  y: "-120%"
+});
